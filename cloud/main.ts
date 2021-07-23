dd1939605c69420bb42a12f41eae486073376770
Robby Helms <robby@pocketprep.com>
2021-07-23 12:37:27 -0400

    Add fetchUsers cloud function

--- cloud/main.ts
+++ cloud/main.ts
@@ -1,3 +1,5 @@
-Parse.Cloud.define('exampleCloudFunction', () => {
-    return 'test'
+Parse.Cloud.define('fetchUsers', async () => {
+    const usersQuery = new Parse.Query(Parse.User)
+    const users = await usersQuery.findAll({ useMasterKey: true })
+    return users
 })
