3e94f4dc9c7680510a749407af2f44cbc48cbe9f
Colin Ulin <colin@pocketprep.com>
2021-09-17 10:46:10 -0400

    added better streaming and 50mb file

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import express, { NextFunction, Request, Response } from 'express'
 // import { ParseServer } from 'parse-server'
 import throng from 'throng'
 import dotenv from 'dotenv'
-import { createReadStream } from 'fs'
+import fs from 'fs'
 import cors from 'cors'
 
 dotenv.config()
@@ -33,13 +33,50 @@ const start = () => {
 
     // Mount streaming path
     app.get('/stream', cors(), (req, res) => {
-        const readStream = createReadStream('./data.txt')
-        readStream.on('data', (data) => {
-            res.write(data)
-        })
-        readStream.on('end', () => {
-            res.status(200).send()
-        })
+        function readBytes (fd: number , sharedBuffer: Buffer) {
+            return new Promise((resolve, reject) => {
+                fs.read(
+                    fd, 
+                    sharedBuffer,
+                    0,
+                    sharedBuffer.length,
+                    null,
+                    (err) => {
+                        if (err) { return reject(err) }
+                        resolve(true)
+                    }
+                )
+            })
+        }
+        
+        async function* generateChunks (filePath: string, size: number) {
+            const sharedBuffer = Buffer.alloc(size)
+            const stats = fs.statSync(filePath) // file details
+            const fd = fs.openSync(filePath, 'r') // file descriptor
+            let bytesRead = 0 // how many bytes were read
+            let end = size 
+            
+            for (let i = 0; i < Math.ceil(stats.size / size); i++) {
+                await readBytes(fd, sharedBuffer)
+                bytesRead = (i + 1) * size
+                if (bytesRead > stats.size) {
+                    // When we reach the end of file, 
+                    // we have to calculate how many bytes were actually read
+                    end = size - (bytesRead - stats.size)
+                }
+                yield sharedBuffer.slice(0, end)
+            }
+        }
+
+        const CHUNK_SIZE = 10000000 // 10MB
+        async function main (filePath: string) {  
+            const chunks = generateChunks(filePath, CHUNK_SIZE)
+            for await (const chunk of chunks) {
+                res.write(chunk)
+            }
+        }
+
+        main('./50mb.txt')
     })
 
     // // Create the Parse Server
