dd5e56fe9d19a3226062ced91c819add7bc4cc1f
Robby Helms <robby@pocketprep.com>
2021-10-14 14:07:11 -0400

    Remove throng

--- index.ts
+++ index.ts
@@ -1,45 +1,37 @@
 import express, { NextFunction, Request, Response } from 'express'
-import throng from 'throng'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import { stream } from './routes/stream'
-import * as backgrounds from './routes/backgrounds'
+import { backgroundRouter } from './routes/backgrounds'
 
 dotenv.config()
 
 const port = process.env.PORT || 5000
 const serverURL = process.env.ROOT_URL || `http://localhost:${port}`
 
-const start = () => {
-    // Create our Express app and force HTTPS
-    const app = express()
-    app.use((req: Request, res: Response, next: NextFunction) => {
-        const host = req.get('host')
-        // The 'x-forwarded-proto' check is for Heroku
-        !req.secure
-        && (req.get('x-forwarded-proto') !== 'https')
-        && (host && !host.includes('localhost'))
-            ? res.redirect('https://' + req.get('host') + req.url)
-            : next()
-    })
-    app.use(cors())
-
-    app.use(express.static('public'))
-
-    // Streaming
-    app.get('/stream', stream)
-
-    // Backgrounds
-    app.post('/backgrounds', backgrounds.create)
-
-    app.listen(port, () => {
-        /* eslint-disable-next-line no-console */
-        console.log(`Started server at ${serverURL}`)
-    })
-}
-
-throng({
-    workers: process.env.WORKERS || 1,
-    lifetime: Infinity,
-    start,
+// Create our Express app and force HTTPS
+const app = express()
+app.use((req: Request, res: Response, next: NextFunction) => {
+    const host = req.get('host')
+    // The 'x-forwarded-proto' check is for Heroku
+    !req.secure
+    && (req.get('x-forwarded-proto') !== 'https')
+    && (host && !host.includes('localhost'))
+        ? res.redirect('https://' + req.get('host') + req.url)
+        : next()
 })
+app.use(cors())
+
+app.use(express.static('public'))
+
+// Streaming
+app.get('/stream', stream)
+
+// Backgrounds
+app.use(backgroundRouter)
+
+app.listen(port, () => {
+    /* eslint-disable-next-line no-console */
+    console.log(`Started server at ${serverURL}`)
+})
+// }
--- routes/backgrounds.ts
+++ routes/backgrounds.ts
@@ -1,22 +1,27 @@
-import { RequestHandler } from 'express'
+import express from 'express'
 
-export const create: RequestHandler = (req, res) => {
+const backgroundRouter = express.Router()
+
+backgroundRouter.post('/backgrounds', (req, res) => {
     // TODO: Process the background
     // TODO: Save the background to Mongo as a single large document
     // TODO: Save the background to Mongo as many small documents
-
     return res.send({
         function: 'create',
         status: 'ok',
     })
-}
+})
 
 // TODO: Add a `fetchAll` handler to fetch backgrounds from Mongo (single document or many documents)
-export const fetchAll: RequestHandler = (req, res) => {
+backgroundRouter.get('/backgrounds', (req, res) => {
     // TODO: Retrieve all the backgrounds from Mongo (those that are single documents or those that are many documents)
-}
+})
 
 // TODO: Add a `fetchById` handler to fetch a single background by its objectId (single document or many documents)
-export const fetchById: RequestHandler = (req, res) => {
+backgroundRouter.get('/backgrounds/:id', (req, res) => {
     // TODO: Retrieve a single background by its objectId (single document or many documents)
+})
+
+export { 
+    backgroundRouter,
 }
