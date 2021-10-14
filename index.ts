d4545f852d4e2cc7558cf3d03e3326729adc3a2f
Robby Helms <robby@pocketprep.com>
2021-10-14 14:54:23 -0400

    Using cors npm package again

--- index.ts
+++ index.ts
@@ -1,31 +1,30 @@
-import express, { NextFunction, Request, Response } from 'express'
+import express from 'express'
 import dotenv from 'dotenv'
-import { stream } from './routes/stream'
-import { backgroundRouter } from './routes/backgrounds'
+import cors from 'cors'
+// import { stream } from './routes/stream'
+// import { backgroundRouter } from './routes/backgrounds'
 
 dotenv.config()
 
 const port = process.env.PORT || 5000
 const serverURL = process.env.ROOT_URL || `http://localhost:${port}`
 
-// Create our Express app and force HTTPS
+// Create our Express app
 const app = express()
-app.use((req: Request, res: Response, next: NextFunction) => {
-    const host = req.get('host')
-    // The 'x-forwarded-proto' check is for Heroku
-    !req.secure
-    && (req.get('x-forwarded-proto') !== 'https')
-    && (host && !host.includes('localhost'))
-        ? res.redirect('https://' + req.get('host') + req.url)
-        : next()
-})
+
+// // Force HTTPS
+// app.use((req, res, next) => {
+//     const host = req.get('host')
+//     // The 'x-forwarded-proto' check is for Heroku
+//     !req.secure
+//     && (req.get('x-forwarded-proto') !== 'https')
+//     && (host && !host.includes('localhost'))
+//         ? res.redirect('https://' + req.get('host') + req.url)
+//         : next()
+// })
 
 // CORS
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-    next()
-})
+app.use(cors())
 
 app.post('/backgrounds', (req, res) => {
     return res.send('ok')
