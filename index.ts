9aee61fb212cea38542f3cf88ab9f5fde9b48fe1
Robby Helms <robby@pocketprep.com>
2021-10-15 11:35:55 -0400

    Force https

--- index.ts
+++ index.ts
@@ -12,16 +12,16 @@ const serverURL = process.env.ROOT_URL || `http://localhost:${port}`
 // Create our Express app
 const app = express()
 
-// // Force HTTPS
-// app.use((req, res, next) => {
-//     const host = req.get('host')
-//     // The 'x-forwarded-proto' check is for Heroku
-//     !req.secure
-//     && (req.get('x-forwarded-proto') !== 'https')
-//     && (host && !host.includes('localhost'))
-//         ? res.redirect('https://' + req.get('host') + req.url)
-//         : next()
-// })
+// Force HTTPS
+app.use((req, res, next) => {
+    const host = req.get('host')
+    // The 'x-forwarded-proto' check is for Heroku
+    !req.secure
+    && (req.get('x-forwarded-proto') !== 'https')
+    && (host && !host.includes('localhost'))
+        ? res.redirect('https://' + req.get('host') + req.url)
+        : next()
+})
 
 // CORS
 app.use(cors())
