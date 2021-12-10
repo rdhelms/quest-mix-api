36a8618216987925c6ee54956ebf2f2b2af8dc31
Robby Helms <robby@pocketprep.com>
2021-12-10 15:23:58 -0500

    Add socket.io and redis adapter

--- index.ts
+++ index.ts
@@ -1,16 +1,35 @@
 import express from 'express'
+import { createServer } from 'http'
+import { Server } from 'socket.io'
+import { createAdapter } from '@socket.io/redis-adapter'
+import { createClient } from 'redis'
 import dotenv from 'dotenv'
-import cors from 'cors'
 import { streamRouter } from './routes/stream'
 import { backgroundRouter } from './routes/backgrounds'
 import { connectToDatabase } from './db'
 
 dotenv.config()
 
-connectToDatabase().then(() => {
-    // Create our Express app
-    const app = express()
+// Create our Express app
+const app = express()
+const httpServer = createServer(app)
+const io = new Server(httpServer, {
+    cors: {
+        origin: true,
+    },
+})
 
+// Setup redis adapter
+const pubClient = createClient({ socket: { host: process.env.REDIS_URL, port: 6379 } })
+const subClient = pubClient.duplicate()
+io.adapter(createAdapter(pubClient, subClient))
+
+// Handle socket events here
+io.on('connection', socket => {
+    console.log(`New client connected: ${socket.id}`)   // eslint-disable-line no-console
+})
+
+connectToDatabase().then(() => {
     // Force HTTPS
     app.use((req, res, next) => {
         const host = req.get('host')
@@ -21,9 +40,6 @@ connectToDatabase().then(() => {
             ? res.redirect('https://' + req.get('host') + req.url)
             : next()
     })
-
-    // CORS
-    app.use(cors())
 
     // Handle Content-Type application/json
     app.use(express.json({
@@ -38,9 +54,8 @@ connectToDatabase().then(() => {
     // Backgrounds
     app.use('/backgrounds', backgroundRouter)
 
-    const port = process.env.PORT || 5000
-
-    app.listen(port, () => {
+    const port = process.env.PORT || 50000
+    httpServer.listen(port, () => {
         /* eslint-disable-next-line no-console */
         console.log(`Server is listening on port ${port}`)
     })
